b0742f0e56cd3a7bf5b9d89b8cc62957388aae9e
Santi Diaz <sdiaz@aidacanarias.com>
2022-01-16 17:49:53 +0000

    Generate golden master output

--- C#/Trivia/Trivia.Tests/GameTest.cs
+++ C#/Trivia/Trivia.Tests/GameTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Xunit;
 
 namespace Trivia.Tests
@@ -5,8 +7,19 @@ namespace Trivia.Tests
     public class GameTest
     {
         [Fact]
-        public void Test1()
+        public void Generate_golden_master_files()
         {
+            const string folder = @"../../../GoldenMaster";
+            Directory.CreateDirectory(folder);
+
+            for (var i = 0; i <= 100; i++)
+            {
+                var file = $"trivialOutput.{i}.txt";
+                using var fileStream = new FileStream(Path.Combine(folder, file), FileMode.Create);
+                using var outputStream = new StreamWriter(fileStream);
+                Console.SetOut(outputStream);
+                GameRunnerNotRamdom.Execute(i);
+            }
         }
     }
 }
---
+++ C#/Trivia/Trivia/GameRunnerNotRamdom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Trivia
+{
+    public static class GameRunnerNotRamdom
+    {
+        private static bool notAWinner;
+
+        public static void Execute(int seed)
+        {
+            var aGame = new Game();
+            aGame.Add("Chet");
+            aGame.Add("Pat");
+            aGame.Add("Sue");
+            var rand = new Random(seed);
+            do
+            {
+                aGame.Roll(rand.Next(5) + 1);
+                notAWinner = rand.Next(9) == 7 ? aGame.WrongAnswer() : aGame.WasCorrectlyAnswered();
+            } while (notAWinner);
+        }
+    }
+}
