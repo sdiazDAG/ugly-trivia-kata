ebfb53f6c1a78fdfedec846878e4364fb028460e
Santi Diaz <sdiaz@aidacanarias.com>
2022-01-16 17:58:12 +0000

    Test to compare that refactoring output is equal to golden master output

--- C#/Trivia/Trivia.Tests/GameTest.cs
+++ C#/Trivia/Trivia.Tests/GameTest.cs
@@ -1,12 +1,13 @@
 using System;
 using System.IO;
+using System.Text;
 using Xunit;
 
 namespace Trivia.Tests
 {
     public class GameTest
     {
-        [Fact]
+        [Fact(Skip = "true")]
         public void Generate_golden_master_files()
         {
             const string folder = @"../../../GoldenMaster";
@@ -19,6 +20,21 @@ namespace Trivia.Tests
                 using var outputStream = new StreamWriter(fileStream);
                 Console.SetOut(outputStream);
                 GameRunnerNotRamdom.Execute(i);
+            }
+        }
+
+        [Fact]
+        public void Compare_that_refactoring_output_is_equal_to_golden_master_output()
+        {
+            for (var i = 0; i <= 100; i++)
+            {
+                var file = $"trivialOutput.{i}.txt";
+
+                var currentOutput = new StringBuilder();
+                Console.SetOut(new StringWriter(currentOutput));
+
+                GameRunnerNotRamdom.Execute(i);
+                Assert.Equal(currentOutput.ToString(), File.ReadAllText($@"../../../GoldenMaster/{file}"));
             }
         }
     }
